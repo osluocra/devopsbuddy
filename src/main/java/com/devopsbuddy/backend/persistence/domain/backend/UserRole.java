f418b2ec39ec4beae7ac49dd336b3bf5170326f4
osluocra <osluocra@icloud.com>
2018-09-21 23:10:38 -0500

    Added JPA entities

--- src/main/java/com/devopsbuddy/backend/persistence/domain/backend/UserRole.java
+++ src/main/java/com/devopsbuddy/backend/persistence/domain/backend/UserRole.java
@@ -4,27 +4,8 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
-
 @Entity
-@Table(name="user_role")
 public class UserRole implements Serializable {
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserRole userRole = (UserRole) o;
-        return Objects.equals(user, userRole.user) &&
-                Objects.equals(role, userRole.role);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = user.hashCode();
-        result = 31 + result + role.hashCode();
-        return result;
-    }
-
     /** The serial version UID for Seriazable Classes **/
 
     public static final long UID = 1L;
@@ -32,14 +13,17 @@ public class UserRole implements Serializable {
     public UserRole(){
 
     }
+
     @Id
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="user_id")
     private User user;
 
+
+
     @Id
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "role_id")
+    @JoinColumn(name="role_id")
     private Role role;
 
 
@@ -57,5 +41,19 @@ public class UserRole implements Serializable {
 
     public void setRole(Role role) {
         this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserRole userRole = (UserRole) o;
+        return Objects.equals(user, userRole.user) &&
+                Objects.equals(role, userRole.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, role);
     }
 }
